b26684ff1a7fe2bf34ff0382ccb87ba74a6b52c0
KSanthanesh <santhanesh_2004@yahoo.com>
2021-06-23 16:48:23 +0000

    organise all the code in html,css and js

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,29 +1,28 @@
 /* jshint esversion: 6 */
- /* globals $:false */ 
+/* globals $:false */
 
 let x = 0;
-
 const time = 1500;
 
 // Array images details
-const images = [ 'assets/images/yellow-rose.jpg',
-'assets/images/black-rose.png',
-'assets/images/pink-rose.jpg',
-'assets/images/purple-rose.jpg',
-'assets/images/red-rose.png',
-'assets/images/white-rose.jpg'
+const images = ['assets/images/yellow-rose.jpg',
+    'assets/images/black-rose.png',
+    'assets/images/pink-rose.jpg',
+    'assets/images/purple-rose.jpg',
+    'assets/images/red-rose.png',
+    'assets/images/white-rose.jpg'
 ];
 
 // changing images automatically
 
 function displayRoseImg() {
-    document.getElementById('image').src= images[x];
-    if(x < images.length - 1) {
+    document.getElementById('image').src = images[x];
+    if (x < images.length - 1) {
         x++;
     } else {
         x = 0;
     }
-    setTimeout(displayRoseImg,time);
+    setTimeout(displayRoseImg, time);
 }
 
 window.onload = displayRoseImg;
--- assets/js/script.js
+++ assets/js/script.js
@@ -10,27 +10,26 @@ let lockBoard = false;
 let cardOptionOne, cardOptionTwo = null;
 let cardOpen = null;
 let totalCardFlip = null;
-
-
-
-
+let firstClick = false;
+let timer;
 
 // data defination
 
 function openCard() {
-  if (lockBoard) 
+  if (lockBoard)
+    return;
+  if (firstClick)
     return;
 
-this.classList.add('flip');
-// first Rose card click
-if (!rotateCard) {
-  [rotateCard, cardOptionOne] = [true, this];
-  return;
-}
-rotateCard = false;
-cardOptionTwo = this;
+  this.classList.add('flip');
+  // first Rose card click
+  if (!rotateCard) {
+    [rotateCard, cardOptionOne] = [true, this];
+    return;
+  }
+  rotateCard = false;
+  cardOptionTwo = this;
   checkMatch();
-
 }
 
 // check the card match
@@ -39,40 +38,37 @@ function checkMatch() {
   let cardMatch = cardOptionOne.dataset.name === cardOptionTwo.dataset.name;
   if (cardMatch) {
     disableRoseCards();
-   
+
   } else {
     unflipRoseCards();
   }
-
 }
-// matched cards
-function disableRoseCards() {
 
+// matched cards
+
+function disableRoseCards() {
   cardOptionOne.removeEventListener('click', openCard);
   cardOptionTwo.removeEventListener('click', openCard);
 
   cardOpen = ++cardOpen;
 
- 
-
   resetBoard();
-   
+
   // once finish the game congrats msg, time and total flip card will shown
   // winning msg will pop up
+
   if (cardOpen === 6) {
     document.querySelector('.win-msg').style.display = "block";
     document.querySelector('.totFlipCount span').innerHTML = totalCardFlip;
-    
   }
-
-
 }
 // for closing window pop up for the winning msg
 
 function closePopup() {
+
   document.querySelector('.win-msg').style.display = "none";
   return;
-}
+};
 
 // if not matched, both the cards will close in 1sec.
 function unflipRoseCards() {
@@ -82,7 +78,6 @@ function unflipRoseCards() {
     cardOptionOne.classList.remove('flip');
     cardOptionTwo.classList.remove('flip');
     resetBoard();
-
   }, 1000);
   return;
 }
@@ -107,24 +102,24 @@ function resetBoard() {
 
 document.querySelectorAll('.rose-card').forEach(card => card.addEventListener('click', openCard));
 
-
 // set the timer for game
 let totalMinCount = document.getElementById('mins');
 let totalSecCount = document.getElementById('secs');
 let sec = 0;
 
 // for 2 digit 
+
 function pad(val) {
   return val > 9 ? val : "0" + val;
 }
 
-let timer = setInterval(function (flipcard) {
+timer = setInterval(function () {
   totalSecCount.innerHTML = pad(++sec % 60);
   totalMinCount.innerHTML = pad(parseInt(sec / 60, 10));
- 
-   
+
   if (cardOpen === 6) {
     clearInterval(timer);
-  }
-}, 1000);
 
+  }
+
+}, 1000);
